0665fb9a008b71527a37236ca1deaea8761368e5
Nasc1mento <reisadryan16@gmail.com>
2022-08-18 00:30:42 -0300

    mais questoes reslvidas

---
+++ exercicio-algoritmo/src/meu/projeto/Programa6.java
@@ -0,0 +1,36 @@
+package meu.projeto;
+
+import java.util.Scanner;
+
+public class Programa6 {
+	static int[] preencher (int numeros[]) {
+
+		Scanner sc = new Scanner (System.in);
+		
+		for (int i = 0; i < numeros.length; i ++) {
+			
+			System.out.println("Digite o numero");
+			numeros[i] = sc.nextInt();
+		}
+		
+		return numeros;
+	}
+	
+	static void reverter (int[] numeros, int tamanho) {
+		
+		if (tamanho >= 0) {
+
+			System.out.println(numeros[tamanho]);
+			reverter (numeros, tamanho - 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		int [] array = new int [10];
+		
+		int [] numeros = preencher (array);
+		
+		reverter (numeros, numeros.length-1);
+	}
+}
---
+++ exercicio-algoritmo/src/meu/projeto/Programa7.java
@@ -0,0 +1,28 @@
+package meu.projeto;
+
+public class Programa7 {
+	 public static void main (String [] args) {
+		
+		
+		byte max = 100;
+		byte min = 0;
+		int [] numeros = new int [100];
+		
+		
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = (int) (Math.random() * (max - min + 1) + min);
+			System.out.println(numeros[i]);
+			
+		}
+		
+		System.out.println("Reverter:");
+		
+		for (int j = numeros.length-1; j >= 0; j--) {
+			 System.out.println(numeros[j]);
+		 }
+		
+	}
+	  
+}
+
+
